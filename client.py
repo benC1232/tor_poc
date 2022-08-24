42aafd3e07044cb03f0a736e7fdc7aa16e0c95ea
ben :) <carmonben@gmail.com>
2022-08-25 02:02:38 +0300

    ITS ALIVE!

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import rsa
 import socket
+
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 5555
 
@@ -9,13 +10,18 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connecting to remote computer 80
 server_address = (SERVER_IP, SERVER_PORT)
 sock.connect(server_address)
+#recieveing keys of rsa
 server_msg = sock.recv(1024)
-server_msg = server_msg.decode()
-key = server_msg
-print("key is: "+key)
-keys = key.split(";")
-publicKey = rsa.PublicKey(int(keys[0]),int(key[1]))
+decoded = str(server_msg, 'utf8')
+n = int(decoded)
+server_msg = sock.recv(1024)
+decoded = str(server_msg, 'utf8')
+e = int(decoded)
+#getting message to send to server
 msg = input()
+#encrypting message
+publicKey = rsa.PublicKey(n, e)
 encMessage = rsa.encrypt(msg.encode(), publicKey)
+#sending message and closing the socket
 sock.sendall(encMessage)
 sock.close()
--- proxy.py
+++ proxy.py
@@ -1,5 +1,6 @@
 import rsa
 import socket
+
 LISTEN_PORT = 5555
 """
 recieving encrypted message from client
@@ -13,21 +14,22 @@ listening_sock.bind(server_address)
 listening_sock.listen(1)
 # Create a new conversation socket
 client_soc, client_address = listening_sock.accept()
-publicKey, privateKey = rsa.newkeys(64)
-print(type(publicKey.n))
-print(type(publicKey.e))
-
-n = str(publicKey.n)
-e = str(publicKey.e)
-msg = n+";"+e
-client_soc.sendall(msg.encode())
+#generate keys for rsa
+publicKey, privateKey = rsa.newkeys(512)
+n = bytes(str(publicKey.n), 'utf-8')
+e = bytes(str(publicKey.e), 'utf-8')
+#send keys to client
+client_soc.sendall(n)
+client_soc.sendall(e)
+#recieveing encrypted message
 client_msg = client_soc.recv(1024)
 client_msg = client_msg
+#closing socket with client
 listening_sock.close()
-decMessage = rsa.decrypt(client_msg, privateKey).decode()
 """
 sending message to server
 """
+
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 8637
 # Create a TCP/IP socket
@@ -35,6 +37,9 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connecting to remote computer 80
 server_address = (SERVER_IP, SERVER_PORT)
 sock.connect(server_address)
+#decrypting and sending message
+decMessage = rsa.decrypt(client_msg, privateKey)
 sock.sendall(decMessage)
-print("message sent to server was: "+msg)
+print("message sent to proxy was: " + str(client_msg))
+print("message sent to server was: " + str(decMessage))
 sock.close()
